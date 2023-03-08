7f6e0c91766faa0199674f14d7f94629dbe6f90a
Antoine Jaunard <antoine.stuff@pm.me>
2023-03-08 15:28:20 +0100

    add example

---
+++ arduino-code/iot_meditation_device_01/env-example.h
@@ -0,0 +1,5 @@
+const char* env_ssid = "wifi_network";
+const char* env_password = "wifi_password";
+const char* env_mqtt_server = "mqtt_server.com";
+const char* env_mqtt_topic_1 = "mqtt_topic_1";
+const char* env_mqtt_topic_2 = "mqtt_topic_2";
